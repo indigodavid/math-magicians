f88b16557ae50920ffd3c03fa7d446374a5225ae
David Vera Castillo <indigodavid@gmail.com>
2022-06-30 11:36:20 -0500

    Remove commented old code

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,23 +3,20 @@ import CalculatorButtons from './CalculatorButtons';
 import CalculatorDisplay from './CalculatorDisplay';
 import calculate from '../logic/calculate';
 
-/* const initialObj = {
-  total: null,
-  next: null,
-  operation: null,
-} */
-
 const Calculator = () => {
   const [obj, setObj] = useState({
     total: null,
     next: null,
     operation: null,
   });
+
   const handleChange = (input) => {
     setObj(calculate(obj, input));
     return obj;
   };
+
   const getDisplayInfo = () => obj;
+
   return (
     <div id="calculator">
       <CalculatorDisplay onDisplay={getDisplayInfo} onKeyEvent={handleChange} />
@@ -27,42 +24,5 @@ const Calculator = () => {
     </div>
   );
 };
-
-/* class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.getDisplayInfo = this.getDisplayInfo.bind(this);
-    this.state = {
-      obj: {
-        total: null,
-        next: null,
-        operation: null,
-      },
-    };
-  }
-
-  handleChange(input) {
-    this.setState((state) => ({
-      obj: calculate(state.obj, input),
-    }));
-    const { obj } = this.state;
-    return obj;
-  }
-
-  getDisplayInfo() {
-    const { obj } = this.state;
-    return obj;
-  }
-
-  render() {
-    return (
-      <div id="calculator">
-        <CalculatorDisplay onDisplay={this.getDisplayInfo} onKeyEvent={this.handleChange} />
-        <CalculatorButtons onClickButtons={this.handleChange} />
-      </div>
-    );
-  }
-} */
 
 export default Calculator;

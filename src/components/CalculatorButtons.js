6ceadd5dcae8f4ee1114ea39fc84d7a15a1298d3
David Vera Castillo <indigodavid@gmail.com>
2022-07-01 11:57:50 -0500

    Refactor buttons as objects to improve readability

--- src/components/CalculatorButtons.js
+++ src/components/CalculatorButtons.js
@@ -1,32 +1,17 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
+import buttonsArray from './ButtonsArray';
 
 class CalculatorButtons extends Component {
-  constructor(props) {
-    super(props);
-    this.buttons = props.buttons;
-  }
-
   handleClick(text) {
     const { onClickButtons } = this.props;
     onClickButtons(text);
   }
 
   render() {
-    const { buttons } = this.props;
-    const innerList = [];
-    buttons.forEach((text, index) => {
-      const id = `button-${index}`;
-      let button;
-      if (text === '0') {
-        button = <button type="button" key={id} className="calculator-button button-zero" onClick={() => this.handleClick(text)}>{text}</button>;
-      } else if (text === '÷' || text === 'x' || text === '-' || text === '+' || text === '=') {
-        button = <button type="button" key={id} className="calculator-button orange" onClick={() => this.handleClick(text)}>{text}</button>;
-      } else {
-        button = <button type="button" key={id} className="calculator-button" onClick={() => this.handleClick(text)}>{text}</button>;
-      }
-      innerList.push(button);
-    });
+    const innerList = buttonsArray.map((button) => (
+      <button type="button" key={button.id} className={button.class} onClick={() => this.handleClick(button.label)}>{button.label}</button>
+    ));
     return (
       <div id="calculator-buttons">
         {innerList}
@@ -36,18 +21,7 @@ class CalculatorButtons extends Component {
 }
 
 CalculatorButtons.propTypes = {
-  buttons: PropTypes.arrayOf(PropTypes.string),
   onClickButtons: PropTypes.func.isRequired,
-};
-
-CalculatorButtons.defaultProps = {
-  buttons: [
-    'AC', '+/-', '%', '÷',
-    '7', '8', '9', 'x',
-    '4', '5', '6', '-',
-    '1', '2', '3', '+',
-    '0', '.', '=',
-  ],
 };
 
 export default CalculatorButtons;

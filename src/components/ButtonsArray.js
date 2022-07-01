ad69c2191949015c6479d510bbddc360b1c9fe48
David Vera Castillo <indigodavid@gmail.com>
2022-07-01 12:01:47 -0500

    Add array in separate file

---
+++ src/components/ButtonsArray.js
@@ -0,0 +1,99 @@
+const buttonsArray = [
+  {
+    label: 'AC',
+    class: 'calculator-button',
+    id: 'button-ac',
+  },
+  {
+    label: '+/-',
+    class: 'calculator-button',
+    id: 'button-sign',
+  },
+  {
+    label: '%',
+    class: 'calculator-button',
+    id: 'button-percent',
+  },
+  {
+    label: '÷',
+    class: 'calculator-button button-orange',
+    id: 'button-divide',
+  },
+  {
+    label: '7',
+    class: 'calculator-button',
+    id: 'button-7',
+  },
+  {
+    label: '8',
+    class: 'calculator-button',
+    id: 'button-8',
+  },
+  {
+    label: '9',
+    class: 'calculator-button',
+    id: 'button-9',
+  },
+  {
+    label: 'x',
+    class: 'calculator-button button-orange',
+    id: 'button-multiply',
+  },
+  {
+    label: '4',
+    class: 'calculator-button',
+    id: 'button-4',
+  },
+  {
+    label: '5',
+    class: 'calculator-button',
+    id: 'button-5',
+  },
+  {
+    label: '6',
+    class: 'calculator-button',
+    id: 'button-6',
+  },
+  {
+    label: '-',
+    class: 'calculator-button button-orange',
+    id: 'button-substract',
+  },
+  {
+    label: '1',
+    class: 'calculator-button',
+    id: 'button-1',
+  },
+  {
+    label: '2',
+    class: 'calculator-button',
+    id: 'button-2',
+  },
+  {
+    label: '3',
+    class: 'calculator-button',
+    id: 'button-3',
+  },
+  {
+    label: '+',
+    class: 'calculator-button button-orange',
+    id: 'button-add',
+  },
+  {
+    label: '0',
+    class: 'calculator-button button-zero',
+    id: 'button-0',
+  },
+  {
+    label: '.',
+    class: 'calculator-button',
+    id: 'button-.',
+  },
+  {
+    label: '=',
+    class: 'calculator-button button-orange',
+    id: 'button-equal',
+  },
+];
+
+export default buttonsArray;

60e9dc8c2a01462a7d63979b9ac6ca2255f3b92d
David Vera Castillo <indigodavid@gmail.com>
2022-07-05 17:00:32 -0500

    Add Navbar module

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/calculator',
+      text: 'Calculator',
+    },
+    {
+      id: 3,
+      path: '/quote',
+      text: 'Quote',
+    },
+  ];
+
+  return (
+    <nav className="navbar">
+      <div className="logo">Math Magicians</div>
+      <ul className="navLinks">
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink to={link.path}>{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;

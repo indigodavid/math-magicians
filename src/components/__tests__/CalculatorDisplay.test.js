1cc0dfe09ddff23c71f41569a94f6417c851a46d
David Vera Castillo <indigodavid@gmail.com>
2022-07-06 16:06:16 -0500

    Add tests for CalculatorDisplay module

---
+++ src/components/__tests__/CalculatorDisplay.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import CalculatorDisplay from '../CalculatorDisplay';
+import calculate from '../../logic/calculate';
+
+describe('Tests for CalculatorDisplay module', () => {
+  let obj = {
+    total: null,
+    next: null,
+    operation: null,
+  }
+
+  const handleChange = (input) => {
+    obj = calculate(obj, input)
+    return obj;
+  };
+
+  const getDisplayInfo = () => obj;
+
+  test('Test input is displayed', () => {
+    render(<CalculatorDisplay onKeyEvent={handleChange} onDisplay={getDisplayInfo} />);
+    const inputText = screen.findByRole('input');
+    expect(inputText).toBeTruthy();
+  });
+  
+  test('Check if the display updates its value', () => {
+    handleChange('1');
+    const { container } = render(<CalculatorDisplay onKeyEvent={handleChange} onDisplay={getDisplayInfo} />);
+    const inputText = container.querySelector('#input-display');
+    expect(inputText.value).toBe('1');
+  });
+})

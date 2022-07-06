6159642209939b755bfc96148848c00f8f67a7c8
David Vera Castillo <indigodavid@gmail.com>
2022-07-06 11:13:11 -0500

    Add test for Home.js

---
+++ src/pages/__tests__/Home.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Home from '../Home';
+
+test('Home renders correctly', () => {
+  const title = 'Welcome to Math Magicians';
+  render(<Home />);
+  expect(screen.getByText(title)).toBeInTheDocument();
+})

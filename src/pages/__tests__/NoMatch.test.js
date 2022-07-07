bf7b5bad881a5f32f17d84d9c8e8abf47c3c4988
judah <87juda@gmail.com>
2022-07-07 12:48:11 -0300

    Add 'Please try other URL.' string test

--- src/pages/__tests__/NoMatch.test.js
+++ src/pages/__tests__/NoMatch.test.js
@@ -3,7 +3,12 @@ import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import NoMatch from '../NoMatch';
 
-test('NoMatch renders correctly', () => {
+test('Displays Not Found correctly', () => {
   const { container } = render(<NoMatch />);
   expect(screen.getByText('Not Found')).toBeInTheDocument();
+});
+
+test('Displays Please try another URL correctly', () => {
+  const { container } = render(<NoMatch />);
+  expect(screen.getByText('Please try other URL.')).toBeInTheDocument();
 });

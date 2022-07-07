43a684e423161d8a268e037edec31281a89342cd
judah <87juda@gmail.com>
2022-07-07 12:54:08 -0300

    Fix linters

--- src/pages/__tests__/NoMatch.test.js
+++ src/pages/__tests__/NoMatch.test.js
@@ -4,17 +4,14 @@ import '@testing-library/jest-dom';
 import NoMatch from '../NoMatch';
 
 test('Displays Not Found correctly', () => {
-  const { container } = render(<NoMatch />);
   expect(screen.getByText('Not Found')).toBeInTheDocument();
 });
 
 test('Displays Please try another URL correctly', () => {
-  const { container } = render(<NoMatch />);
   expect(screen.getByText('Please try other URL.')).toBeInTheDocument();
 });
 
 test('Displays 404 ERROR correctly', () => {
-  const { container } = render(<NoMatch />);
   expect(screen.getByText('404 ERROR')).toBeInTheDocument();
 });
 
